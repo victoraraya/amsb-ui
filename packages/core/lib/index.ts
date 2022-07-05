682b03b99cfbdd4341b8964f7e29ceeb7cfabf9f
Victor Araya <victor.araya@globant.com>
2022-07-04 18:49:49 -0600

    add theme to core

--- packages/core/lib/index.ts
+++ packages/core/lib/index.ts
@@ -1,4 +1,5 @@
 import Button from "@amsb-ui/button";
 import Typography from "@amsb-ui/typography";
+import AirMilesThemeProvider from "@amsb-ui/theme";
 
-export { Button, Typography };
+export { Button, Typography, AirMilesThemeProvider };

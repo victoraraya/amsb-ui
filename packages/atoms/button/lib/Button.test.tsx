a491672e716ad5ac8c182165f46ac5c5ee468c45
Victor Araya <victor.araya@globant.com>
2022-07-05 20:08:29 -0600

    add jest

---
+++ packages/atoms/button/lib/Button.test.tsx
@@ -0,0 +1,14 @@
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+import Button from "./Button";
+
+it("should render successfully", () => {
+  const { asFragment, getByText } = render(<Button>Hey!</Button>);
+
+  const button = getByText("Hey!");
+
+  expect(button).toBeInTheDocument();
+
+  expect(asFragment()).toMatchSnapshot();
+});

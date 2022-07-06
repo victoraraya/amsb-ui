87b7cea595c1980307f370f8fb632eecdfd22ea9
Victor Araya <victor.araya@globant.com>
2022-07-05 20:06:54 -0600

    update stories

--- packages/atoms/button/lib/Button.stories.tsx
+++ packages/atoms/button/lib/Button.stories.tsx
@@ -1,12 +1,17 @@
-import AirMilesThemeProvider from "@amsb-ui/theme";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
 import Button from "./Button";
+import AirMilesThemeProvider from "@amsb-ui/theme";
 
-export default { title: "Button" };
+export default {
+  title: "Design System/Atoms/Button",
+  component: Button,
+} as ComponentMeta<typeof Button>;
 
-export const primary = () => (
+const Template: ComponentStory<typeof Button> = () => (
   <AirMilesThemeProvider>
-    <Button color="primary" variant="contained">
-      Hello Button
-    </Button>
+    <Button variant="contained">Button</Button>
   </AirMilesThemeProvider>
 );
+
+export const Primary = Template.bind({});
--- packages/atoms/typography/lib/Typography.stories.tsx
+++ packages/atoms/typography/lib/Typography.stories.tsx
@@ -1,7 +1,22 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Typography from "./Typography";
+import AirMilesThemeProvider from "@amsb-ui/theme";
 
-export default { title: "Typography" };
+export default {
+  title: "Design System/Atoms/Typography",
+  component: Typography,
+} as ComponentMeta<typeof Typography>;
 
-export const primary = () => (
-  <Typography variant="h1">Hello Typography</Typography>
+const Template: ComponentStory<typeof Typography> = () => (
+  <AirMilesThemeProvider>
+    <Typography variant="h1">Almost before we knew it, we had...</Typography>
+    <Typography variant="h2">Almost before we knew it, we had...</Typography>
+    <Typography variant="h3">Almost before we knew it, we had...</Typography>
+    <Typography variant="h4">Almost before we knew it, we had...</Typography>
+    <Typography variant="h5">Almost before we knew it, we had...</Typography>
+    <Typography variant="h6">Almost before we knew it, we had...</Typography>
+  </AirMilesThemeProvider>
 );
+
+export const Titleh1 = Template.bind({});
+Titleh1.storyName = "Titles";
